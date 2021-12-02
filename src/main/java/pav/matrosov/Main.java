93de9317db14b494359179c601cbcc0a8a37af92
Pavel <89654104202@mail.ru>
2021-12-02 16:16:00 +0300

    fix

--- src/main/java/pav/matrosov/Main.java
+++ src/main/java/pav/matrosov/Main.java
@@ -1,37 +1,21 @@
 package pav.matrosov;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws Exception{
         Functions calc = new Functions();
-        Scanner s = new Scanner(System.in);
-        System.out.println("Введите два числа");
-            double a = s.nextDouble();
-            double b = s.nextDouble();
-
+        try (Scanner s = new Scanner(System.in)) {
+            System.out.println("Введите два числа");
+            calc.setA(s.nextDouble());
+            calc.setB(s.nextDouble());
             System.out.println("Введите арифметиское действие (‘+’, ‘-’, ‘*’ или ‘/’)");
-            switch (s.next().charAt(0)) {
-                case '+':
-                    System.out.println(calc.sum(a, b));
-                    break;
-                case '-':
-                    System.out.println(calc.subtraction(a, b));
-                    break;
-                case '*':
-                    System.out.println(calc.multiplication(a, b));
-                    break;
-                case '/':
-                        System.out.println(calc.div(a, b));
-                    if (b == 0) {
-                        throw new IllegalArgumentException("На ноль делить нельзя");
-                    }
-                    break;
-                default:
-                    throw new IllegalArgumentException("Вы ввели некоретный оператор");
-            }
-            s.close();
+            calc.setOperation((s.next()));
+        } catch (Functions.IncorrectOperator incorrectOperator) {
+            System.out.println("Вы ввели некоректный оператор");
+        } catch (Functions.DivWithZero divWithZero) {
+            System.out.println("На ноль делить нельзя");
+        }
     }
 }
